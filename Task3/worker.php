facba5cfe339bdef43441d9ec561541507eac524
Kseniya.Edkova <Kseniya.Edkova@tatar.ru>
2020-11-26 13:30:01 +0300

    add task3

---
+++ Task3/worker.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->queue_declare('task_queue', false, true, false, false);
+
+echo "Waiting\n";
+
+$callback = function ($msg) {
+    $text = $msg->body;
+    echo 'Get ', $text, "\n";
+    sleep(substr_count($msg->body, '.'));
+    $rand = rand(1, 100);
+    $data = fopen("info".$rand.".txt", 'a');
+    fwrite($data, $text."\n");
+    fclose($data);
+
+
+    echo "Finished\n";
+    $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+};
+
+$channel->basic_qos(null, 1, null);
+$channel->basic_consume('task_queue', '', false, false, false, false, $callback);
+
+while ($channel->is_consuming()) {
+    $channel->wait();
+}
+
+$channel->close();
+$connection->close();
