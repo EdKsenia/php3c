db259add6ed5f9774e67fba9ffdc51e207c34a65
Kseniya.Edkova <Kseniya.Edkova@tatar.ru>
2020-11-26 12:27:03 +0300

    add task1

---
+++ Task1/curl.php
@@ -0,0 +1,13 @@
+<?php
+$curl = curl_init();
+$name = "Ksenia";
+$info = "Hi hi hi";
+$data = array("name" => $name, "info" => $info);
+$user_agent = "user_agent: Chrome";
+curl_setopt($curl, CURLOPT_URL, "http://127.0.0.1:8000");
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+curl_setopt($curl, CURLOPT_USERAGENT, $user_agent);
+curl_exec($curl);
+curl_close($curl);
+
